457ef9f96b6fb1ea4350534e1a0e57e042debfe7
ryooit <ryooit70@gmail.com>
2021-03-30 11:04:33 +0900

    Add image logging to PyTorch mnist example

--- mnist/pytorch/main.py
+++ mnist/pytorch/main.py
@@ -77,6 +77,7 @@ def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
+    test_images = []
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
@@ -84,12 +85,18 @@ def test(model, device, test_loader):
             test_loss += F.nll_loss(output, target, reduction='sum').item()
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
+            test_images.append(savvihub.Image(
+                data[0], caption="Pred: {} Truth: {}".format(pred[0].item(), target[0])))
 
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+    savvihub.log({
+        "Examples": test_images,
+    })
 
 
 def save(model, path):
