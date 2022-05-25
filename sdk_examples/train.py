7ef37b2f22f06137d75abd749e26f6fb0bfcd191
Jaeman An <ajmbell@gmail.com>
2022-05-25 11:48:06 +0900

    Add custom resource example & Local file uplaod example

--- sdk_examples/train.py
+++ sdk_examples/train.py
@@ -5,8 +5,38 @@ import time
 access_token = os.environ.get("VESSL_ACCESS_TOKEN")
 
 vessl.vessl_api.set_access_token(access_token)
-vessl.vessl_api.set_organization("floyd")
-vessl.vessl_api.set_project("vessl-mnist-examples")
+vessl.vessl_api.set_organization("jaeman")
+vessl.vessl_api.set_project("examples2")
+
+
+def run_with_local_file_upload(dataset, command):
+    experiment = vessl.create_experiment(
+        cluster_name="aws-apne2-prod1",
+        start_command=command,
+        kernel_resource_spec_name="v1.cpu-2.mem-6",
+        kernel_image_url="public.ecr.aws/vessl/kernels:py36.full-cpu",
+        local_files=["/Users/kuss/Workspace/savvi/examples/:/root/local/"],
+        dataset_mounts=[f"/input/mnist:{dataset}"],
+        working_dir="/root/local",
+    )
+    print(experiment)
+
+
+def run_with_custom_resource(dataset, command):
+    experiment = vessl.create_experiment(
+        cluster_name="vessl-dgx",
+        start_command=command,
+        processor_type="GPU",
+        cpu_limit=2,
+        memory_limit="4Gi",
+        gpu_type="Any",
+        gpu_limit=1,
+        kernel_image_url="public.ecr.aws/vessl/kernels:py36.full-gpu",
+        dataset_mounts=[f"/input/mnist:{dataset}"],
+        local_files=["/Users/kuss/Workspace/savvi/examples/:/root/local/"],
+        working_dir="/root/local/",
+    )
+    print(experiment)
 
 
 def run(dataset, command, commit):
@@ -15,7 +45,7 @@ def run(dataset, command, commit):
         start_command=command,
         kernel_resource_spec_name="v1.cpu-2.mem-6",
         kernel_image_url="public.ecr.aws/vessl/kernels:py36.full-cpu",
-        git_ref_mounts=[f"/root/examples:github/vessl-ai/examples/{commit}"],
+        git_ref_mounts=[f"/root/examples:github/support-vessl/examples/{commit}"],
         dataset_mounts=[f"/input/mnist:{dataset}"],
         working_dir="/root/examples",
     )
@@ -33,8 +63,16 @@ def run(dataset, command, commit):
 
 
 if __name__ == "__main__":
-    run(
+    run_with_custom_resource(
         dataset="floyd/mnist-s3-test",
         command="pip install -r mnist/keras/requirements.txt && python mnist/keras/main.py --save-model --save-image",
-        commit="master",
     )
+    #run_with_local_file_upload(
+    #    dataset="floyd/mnist-s3-test",
+    #    command="pip install -r mnist/keras/requirements.txt && python mnist/keras/main.py --save-model --save-image",
+    #)
+    # run(
+    #     dataset="floyd/mnist-s3-test",
+    #     command="pip install -r mnist/keras/requirements.txt && python mnist/keras/main.py --save-model --save-image",
+    #     commit="master",
+    # )
