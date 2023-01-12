c5935f1335ce31dbd87f939d780696b3037c9279
ryooit <ryooit70@gmail.com>
2023-01-12 13:07:10 +0900

    Remove redundant model and refactoring sasrec code

--- recommenders/sasrec/model.py
+++ recommenders/sasrec/model.py
@@ -1,12 +1,13 @@
-import tensorflow as tf
-from recommenders.utils.timer import Timer
-from tqdm import tqdm
-import vessl
 import os
 import numpy as np
+import tensorflow as tf
+import vessl
 
+from recommenders.utils.timer import Timer
 from recommenders.models.sasrec.model import SASREC
 from recommenders.models.sasrec.ssept import SSEPT
+
+from tqdm import tqdm
 
 
 class VesslLogger:
@@ -24,7 +25,7 @@ class VesslLogger:
         if metric not in self._log:
             self._log[metric] = []
         self._log[metric].append(value)
-        vessl.log(step = step, payload = {
+        vessl.log(step=step, payload={
             metric: value,
         })
 
@@ -111,7 +112,7 @@ class SASREC_Vessl(SASREC)  :
         t0.start()
 
         max_ndgc = 0
-        t_test=  None
+        t_test = None
         for epoch in range(1, num_epochs + 1):
 
             train_loss.reset_states()
@@ -156,7 +157,6 @@ class SASREC_Vessl(SASREC)  :
         return t_test
 
     def predict_next(self, input):
-
         # seq generation
         training = False
         seq = np.zeros([self.seq_max_len], dtype=np.int32)
@@ -168,14 +168,12 @@ class SASREC_Vessl(SASREC)  :
             if idx == -1:
                 break
 
-        user = None
         input_seq = np.array([seq])
         candidate = np.expand_dims(np.arange(1, self.item_num+1, 1), axis=0)
 
         mask = tf.expand_dims(tf.cast(tf.not_equal(input_seq, 0), tf.float32), -1)
         seq_embeddings, positional_embeddings = self.embedding(input_seq)
         seq_embeddings += positional_embeddings
-        # seq_embeddings = self.dropout_layer(seq_embeddings)
         seq_embeddings *= mask
         seq_attention = seq_embeddings
         seq_attention = self.encoder(seq_attention, training, mask)
@@ -188,8 +186,6 @@ class SASREC_Vessl(SASREC)  :
         candidate_emb = tf.transpose(candidate_emb, perm=[0, 2, 1])  # (b, d, s)
 
         test_logits = tf.matmul(seq_emb, candidate_emb)
-
-
         test_logits = tf.reshape(
             test_logits,
             [tf.shape(input_seq)[0], self.seq_max_len, self.item_num],
@@ -202,153 +198,6 @@ class SASREC_Vessl(SASREC)  :
         return item_pred
 
     def save_upload(self, save_path, epoch):
-        #output_test/epoch_1.data-000... , json
-
-        self.save_weights(str(os.path.join(save_path, 'epoch_{}'.format(epoch))))
-        self.load_weights(str(os.path.join(save_path, 'epoch_{}'.format(epoch))))
-        vessl.upload(str(os.path.join(save_path, 'epoch_{}.data-00000-of-00001'.format(epoch))))
-        vessl.upload(str(os.path.join(save_path, 'epoch_{}.index'.format(epoch))))
-
-
-
-class SSEPT_Vessl(SSEPT) :
-
-    def __init__(self, **kwargs ):
-        super().__init__(**kwargs)
-
-    def train(self, dataset, sampler, **kwargs):
-        """
-        High level function for model training as well as
-        evaluation on the validation and test dataset and
-        for logging training and from source code of recommeders
-
-        :param model: model to train
-        :param dataset: dataset
-        :param sampler: sampler
-        :param kwargs:
-        :return:
-        """
-
-        if kwargs['save_path'] is not None and not os.path.isdir(kwargs['save_path']):
-            os.mkdir(kwargs['save_path'])
-
-        vessllogger = VesslLogger()
-
-        num_epochs = kwargs.get("num_epochs", 10)
-        batch_size = kwargs.get("batch_size", 128)
-        lr = kwargs.get("learning_rate", 0.001)
-        val_epoch = kwargs.get("val_epoch", 5)
-
-        num_steps = int(len(dataset.user_train) / batch_size)
-
-        optimizer = tf.keras.optimizers.Adam(
-            learning_rate=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-7
-        )
-
-        loss_function = self.loss_function
-
-        train_loss = tf.keras.metrics.Mean(name="train_loss")
-
-        train_step_signature = [
-            {
-                "users": tf.TensorSpec(shape=(None, 1), dtype=tf.int64),
-                "input_seq": tf.TensorSpec(
-                    shape=(None, self.seq_max_len), dtype=tf.int64
-                ),
-                "positive": tf.TensorSpec(
-                    shape=(None, self.seq_max_len), dtype=tf.int64
-                ),
-                "negative": tf.TensorSpec(
-                    shape=(None, self.seq_max_len), dtype=tf.int64
-                ),
-            },
-            tf.TensorSpec(shape=(None, 1), dtype=tf.int64),
-        ]
-
-        @tf.function(input_signature=train_step_signature)
-        def train_step(inp, tar):
-            with tf.GradientTape() as tape:
-                pos_logits, neg_logits, loss_mask = self(inp, training=True)
-                loss = loss_function(pos_logits, neg_logits, loss_mask)
-
-            gradients = tape.gradient(loss, self.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-
-            # vessl.hp.lr = optimizer.learning_rate
-            # vessl.hp.update()
-
-            train_loss(loss)
-            return loss
-
-        T = 0.0
-        t0 = Timer()
-        t0.start()
-
-        max_ndgc = 0
-        for epoch in range(1, num_epochs + 1):
-
-            train_loss.reset_states()
-
-            for step in tqdm(range(num_steps), total=num_steps, ncols=70, leave=False, unit="b"):
-                u, seq, pos, neg = sampler.next_batch()
-
-                inputs, target = self.create_combined_dataset(u, seq, pos, neg)
-
-                loss = train_step(inputs, target)
-
-                vessllogger.log(step + (epoch - 1) * num_steps, 'loss', loss)
-
-            self.save_upload(kwargs['save_path'], epoch)
-            if epoch % val_epoch == 0 or epoch == num_epochs:
-                t0.stop()
-                t1 = t0.interval
-                T += t1
-                print("Evaluating...")
-                t_test = self.evaluate(dataset)
-                t_valid = self.evaluate(dataset)
-                print(f"\nepoch: {epoch}, time: {T}, valid (NDCG-10: {t_valid[0]}, HR-10: {t_valid[1]})")
-                vessllogger.log(epoch, 'val_NDCG-10', t_valid[0])
-                vessllogger.log(epoch, 'val_HR-10', t_valid[1])
-                print(f"epoch: {epoch}, time: {T},  test (NDCG-10: {t_test[0]}, HR-10: {t_test[1]})")
-                vessllogger.log(epoch, 'test_NDCG-10', t_test[0])
-                vessllogger.log(epoch, 'test_HR-10', t_test[1])
-
-                if max_ndgc < t_test[1] and kwargs['save_path'] is not None:
-                    max_ndgc = t_test[1]
-
-                    self.save_weights(str(os.path.join(kwargs['save_path'], 'best')))
-                    vessl.upload(str(os.path.join(kwargs['save_path'], 'best')))
-
-                t0.start()
-
-        self.load_weights(str(os.path.join(kwargs['save_path'] + 'best')))
-        return t_test
-
-    def predict_next(self, input):
-
-        #seq generation
-        seq = np.zeros([self.seq_max_len], dtype=np.int32)
-        idx = self.seq_max_len - 1
-        idx -= 1
-        for i in reversed(input):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1:
-                break
-        inputs = {}
-        inputs["user"] = np.expand_dims(np.array(np.randint(1, self.user_num)), axis=0)  ## should be changed
-        inputs["input_seq"] = np.array([seq])
-        inputs["candidate"] = np.expand_dims(np.arange(1, self.item_num, 1), axis=0)
-
-        predictions = self.predict(inputs)
-        predictions = np.array(predictions)[0]
-
-        item_pred = predictions.argmax() + 1
-
-        return item_pred
-
-    def save_upload(self, save_path, epoch):
-
         self.save_weights(str(os.path.join(save_path, 'epoch_{}'.format(epoch))))
         self.load_weights(str(os.path.join(save_path, 'epoch_{}'.format(epoch))))
         vessl.upload(str(os.path.join(save_path, 'epoch_{}.data-00000-of-00001'.format(epoch))))
