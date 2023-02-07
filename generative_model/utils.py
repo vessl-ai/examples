e1de1941539283553345c742b06c6ce4e9fc18c6
treasuraid <jinpil@vessl.ai>
2023-02-07 14:14:36 +0900

    readme add

--- generative_model/utils.py
+++ generative_model/utils.py
@@ -44,47 +44,8 @@ class VesslLogger:
         vessl.log({title :
                 self._examples})
 
-
-def save_model_card(repo_name, images=None, base_model=str, dataset_name=str, repo_folder=None):
-    img_str = ""
-    for i, image in enumerate(images):
-        image.save(os.path.join(repo_folder, f"image_{i}.png"))
-        img_str += f"![img_{i}](./image_{i}.png)\n"
-
-    yaml = f"""
----
-license: creativeml-openrail-m
-base_model: {base_model}
-tags:
-- stable-diffusion
-- stable-diffusion-diffusers
-- text-to-image
-- diffusers
-- lora
-inference: true
----
-    """
-    model_card = f"""
-# LoRA text2image fine-tuning - {repo_name}
-These are LoRA adaption weights for {base_model}. The weights were fine-tuned on the {dataset_name} dataset. You can find some example images in the following. \n
-{img_str}
-"""
-    with open(os.path.join(repo_folder, "README.md"), "w") as f:
-        f.write(yaml + model_card)
-
-def get_full_repo_name(model_id: str, organization: Optional[str] = None, token: Optional[str] = None):
-    if token is None:
-        token = HfFolder.get_token()
-    if organization is None:
-        username = whoami(token)["name"]
-        return f"{username}/{model_id}"
-    else:
-        return f"{organization}/{model_id}"
-
-
 DATASET_NAME_MAPPING = {
-    "VESSL/Bored_Ape_NFT" : ("image", "text"),
-    "lambdalabs/pokemon-blip-captions": ("image", "text"),
+    "VESSL/Bored_Ape_NFT_text" : ("image", "text"),
 }
 
 def print_num_param(model, name) :
