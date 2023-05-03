ebc9df5d17bb96f673a30797246057abfaad834f
Jinpil Choi <jinpil@vessl.ai>
2023-05-03 13:01:52 +0900

    fix

--- langchain/app.py
+++ langchain/app.py
@@ -4,26 +4,21 @@ from test import *
 def make_qa_from_git_streamlit() :
     return make_qa_from_git(None)
 
-# Streamlit 앱 시작
-qa = make_qa_from_git_streamlit()
+def main() :
+    qa = make_qa_from_git_streamlit()
 
-st.title("Vessl docs searcher")
-st.write("You can get answers by question and answering from vessl docs.")
+    st.title("Vessl docs searcher")
+    st.write("You can get answers by question and answering from vessl docs.")
 
-# QA 모델 불러오기
-st.write("Loading QA model...")
-st.write("Done!")
-# 사용자로부터 질문 입력 받기
-user_question = st.text_input("Enter Question:")
+    user_question = st.text_input("Enter Question:")
+    submit_button = st.button("Submit Question")
 
-# 질문 제출 버튼
-submit_button = st.button("Submit Question")
+    if submit_button and user_question:
+        # 질문에 대한 답변 얻기
+        st.write("Question: " + user_question)
+        st.write("Processing..")
+        answer = qa.run(user_question)
+        st.write(answer)
 
-if submit_button and user_question:
-    # 질문에 대한 답변 얻기
-    st.write("Question: " + user_question)
-
-    st.write("Processing..")
-    answer = qa.run(user_question)
-
-    st.write(answer)
+if __name__ == "__main__" :
+    main()
