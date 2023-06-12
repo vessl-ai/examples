e19b7f949545e626dcf08c175d937168646a3f85
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-06-12 13:48:42 +0900

    try stable diffusion 2 by Stability AI

--- stable-diffusion/main.py
+++ stable-diffusion/main.py
@@ -1,6 +1,6 @@
 import torch
 import streamlit as st
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 VESSL_LOGO_URL = (
     "https://vessl-public-apne2.s3.ap-northeast-2.amazonaws.com/vessl-logo/vessl-ai_color_light"
@@ -9,11 +9,13 @@ VESSL_LOGO_URL = (
 
 st.set_page_config(layout="wide")
 st.image(VESSL_LOGO_URL, width=400)
-intro = ['VESSL AI: Manage your own Stable Diffusion session!', 
-'Setting environment is one of the biggest bottleneck for machine learning projects. 😥',
-'VESSL AI lets you overcome the bottleneck with the use of yaml. 📋',
-'By providing yaml, you can declaratively run your machine learning projects in reliable manner!',
-'Try your own Stable Diffusion session with simple yaml we provide. 🚀']
+intro = [
+    "VESSL AI: Manage your own Stable Diffusion session!",
+    "Setting environment is one of the biggest bottleneck for machine learning projects. 😥",
+    "VESSL AI lets you overcome the bottleneck with the use of yaml. 📋",
+    "By providing yaml, you can declaratively run your machine learning projects in reliable manner!",
+    "Try your own Stable Diffusion session with simple yaml we provide. 🚀",
+]
 
 st.title("VESSL AI: Manage your own Stable Diffusion session!")
 for e in intro:
@@ -22,9 +24,10 @@ for e in intro:
 
 
 # Load model
-pipe = StableDiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16
-)
+model_id = "stabilityai/stable-diffusion-2-1"
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
 with st.form("prompt", clear_on_submit=False):
@@ -57,10 +60,19 @@ interactive:
   ports:
     - 8501
     """
-    st.markdown(f'<p style="font-family:Courier; color:Black; font-size: 20px;">Here, we provide the yaml we used for setting up this streamlit session.</p>', unsafe_allow_html=True)
+    st.markdown(
+        f'<p style="font-family:Courier; color:Black; font-size: 20px;">Here, we provide the yaml we used for setting up this streamlit session.</p>',
+        unsafe_allow_html=True,
+    )
     st.code(yaml, language="yaml", line_numbers=False)
 with col5:
-        st.markdown(f'<p style="font-family:Courier; color:Black; font-size: 20px;">You can save the yaml and run it by userself! Try:</p>', unsafe_allow_html=True)
-        st.code( "pip install vessl\nvessl run -f youryaml", language="python", line_numbers=False)
+    st.markdown(
+        f'<p style="font-family:Courier; color:Black; font-size: 20px;">You can save the yaml and run it by userself! Try:</p>',
+        unsafe_allow_html=True,
+    )
+    st.code("pip install vessl\nvessl run -f youryaml", language="python", line_numbers=False)
 
-st.markdown(f'<p style="font-family:Courier; color:Black; font-size: 20px;">For further details, visit <a href="https://vessl.ai/">link!</a>', unsafe_allow_html=True)
+st.markdown(
+    f'<p style="font-family:Courier; color:Black; font-size: 20px;">For further details, visit <a href="https://vessl.ai/">link!</a>',
+    unsafe_allow_html=True,
+)
