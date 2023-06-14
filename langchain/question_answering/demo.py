a210117b5f1c4cb15758b492780c3d95f87c1203
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-06-14 12:27:58 +0900

    edit langchain streamlit session

--- langchain/question_answering/demo.py
+++ langchain/question_answering/demo.py
@@ -3,27 +3,42 @@ from utils import *
 from streamlit_chat import message as msg
 import os
 
-VESSL_LOGO_URL = "https://vessl-public-apne2.s3.ap-northeast-2.amazonaws.com/vessl-logo/vessl-ai_color_light" \
-                 "-background.png"
+VESSL_LOGO_URL = (
+    "https://vessl-public-apne2.s3.ap-northeast-2.amazonaws.com/vessl-logo/vessl-ai_color_light"
+    "-background.png"
+)
 st.set_page_config(layout="wide")
 
 
 @st.cache_resource()
 def MakingQA(repo_url=None, branch="main", files=None, chunk_size=500, chunk_overlap=100):
-    st.write("Making QA Chatbot Server from your data with VESSL, it may take a few minutes depends on your data size")
-    return make_qa(repo_url,
-                   branch,
-                   files,
-                   file_dir=None,
-                   chunk_size=chunk_size,
-                   chunk_overlap=chunk_overlap,
-                   )
+    st.write(
+        "Making QA Chatbot Server from your data with VESSL, it may take a few minutes depends on your data size"
+    )
+    return make_qa(
+        repo_url,
+        branch,
+        files,
+        file_dir=None,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
 
 
 def streamlit_display_title():
     st.image(VESSL_LOGO_URL, width=400)
-    st.title("Making QA Chatbot Server from your data with VESSL")
-    st.write("VESSL is End-to-End MLops platform for ML engineers. Please check our product at https://vessl.ai/")
+    intro = [
+        "VESSL AI: Manage your own Stable Diffusion session!",
+        "Setting environment is one of the biggest bottleneck for machine learning projects. 😥",
+        "VESSL AI lets you overcome the bottleneck with the use of yaml. 📋",
+        "By providing yaml, you can declaratively run your machine learning projects in reliable manner!",
+        "Try your own Stable Diffusion session with simple yaml we provide. 🚀",
+    ]
+
+    st.title("VESSL AI: Manage your own Langchain session!")
+    for e in intro:
+        _e = f'<p style="font-family:Courier; color:Black; font-size: 20px;">{e}</p>'
+        st.markdown(_e, unsafe_allow_html=True)
     return
 
 
@@ -46,15 +61,20 @@ def main():
 
     if "uploaded_files" not in st.session_state and "git_repo" not in st.session_state:
         st.subheader("2. Please upload your files or enter git repository url")
-        uploaded_files = st.file_uploader("Upload files",
-                                          accept_multiple_files=True,
-                                          type=["txt", "pdf", "docx", "py", "c", "h"]
-                                          )
-        repo_url = st.text_input("Enter git repo url to integrate in your source", value="https://github.com/vessl-ai"
-                                                                                         "/gitbook-docs.git")
-        repo_branch = st.text_input("Enter branch of repo url to integrate in your source", value="main")
+        uploaded_files = st.file_uploader(
+            "Upload files", accept_multiple_files=True, type=["txt", "pdf", "docx", "py", "c", "h"]
+        )
+        repo_url = st.text_input(
+            "Enter git repo url to integrate in your source",
+            value="https://github.com/vessl-ai" "/gitbook-docs.git",
+        )
+        repo_branch = st.text_input(
+            "Enter branch of repo url to integrate in your source", value="main"
+        )
         chunk_size = st.number_input("Enter chunk size [How large to split long texts]", value=500)
-        chunk_overlap = st.number_input("Enter chunk overlap [How much to overlap splitted texts]", value=100)
+        chunk_overlap = st.number_input(
+            "Enter chunk overlap [How much to overlap splitted texts]", value=100
+        )
         submit_button = st.button("Submit", key="submit_button")
         if submit_button and (uploaded_files or repo_url) and chunk_size and chunk_overlap:
             file_paths = []
@@ -74,7 +94,7 @@ def main():
             st.write("click submit button again to move next step")
             quit()
 
-    if st.session_state.get('uploaded_files') is None and st.session_state.get('git_repo') is None:
+    if st.session_state.get("uploaded_files") is None and st.session_state.get("git_repo") is None:
         st.warning("Please upload files or enter git repo url")
         quit()
 
@@ -90,28 +110,65 @@ def main():
     submit_button = st.button("Submit Question")
 
     if "history" not in st.session_state:
-        st.session_state['history'] = []
+        st.session_state["history"] = []
 
     # ensure displaying history
-    for hist in st.session_state['history']:
+    for hist in st.session_state["history"]:
         msg(hist[0], is_user=True)
         msg(hist[1], is_user=False)
 
     if submit_button and user_question:
         if user_question == "clear":
-            st.session_state['history'] = []
+            st.session_state["history"] = []
             st.success("History cleared")
             quit()
         msg(user_question, is_user=True)
         try:
             msg("Processing your question...", is_user=False)
-            result = qa({"question": user_question, "chat_history": st.session_state['history']})
+            result = qa({"question": user_question, "chat_history": st.session_state["history"]})
         except:
             msg("Error occured while processing, please retry with fresh question", is_user=False)
             return
-        st.session_state['history'].append((user_question, result["answer"]))
+        st.session_state["history"].append((user_question, result["answer"]))
         # display new answer
         msg(result["answer"], is_user=False)
+
+    col4, col5 = st.columns(2)
+    with col4:
+        yaml = """name : langchain
+resources:
+  cluster: aws-apne2-prod1
+  cpu: 1
+  memory: 16Gi
+  disk: 10Gi
+image: quay.io/vessl-ai/kernels:py38-202303150331
+run:
+  - workdir: /root/examples/langchain/question_answering/
+    command: |
+      bash ./run.sh
+volumes:
+  /root/examples: git://github.com/vessl-ai/examples
+interactive:
+  runtime: 24h
+  ports:
+    - 8501
+        """
+        st.markdown(
+            f'<p style="font-family:Courier; color:Black; font-size: 20px;">Here, we provide the yaml we used for setting up this streamlit session.</p>',
+            unsafe_allow_html=True,
+        )
+        st.code(yaml, language="yaml", line_numbers=False)
+    with col5:
+        st.markdown(
+            f'<p style="font-family:Courier; color:Black; font-size: 20px;">You can save the yaml and run it by userself! Try:</p>',
+            unsafe_allow_html=True,
+        )
+        st.code("pip install vessl\nvessl run -f youryaml", language="python", line_numbers=False)
+
+    st.markdown(
+        f'<p style="font-family:Courier; color:Black; font-size: 20px;">For further details, visit <a href="https://vessl.ai/">this link!</a>',
+        unsafe_allow_html=True,
+    )
 
 
 if __name__ == "__main__":
