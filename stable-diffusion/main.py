29dc29a10558321a1ddfa1610214215def0101c7
Floyd Ryoo <floyd@vessl.ai>
2023-06-19 18:52:45 -0700

    Update stable-diffusion texts

--- stable-diffusion/main.py
+++ stable-diffusion/main.py
@@ -10,16 +10,16 @@ VESSL_LOGO_URL = (
 st.set_page_config(layout="wide")
 st.image(VESSL_LOGO_URL, width=400)
 intro = [
-    "VESSL AI: Manage your own Stable Diffusion session!",
-    "Setting environment is one of the biggest bottleneck for machine learning projects. 😥",
-    "VESSL AI lets you overcome the bottleneck with the use of yaml. 📋",
-    "By providing yaml, you can declaratively run your machine learning projects in reliable manner!",
-    "Try your own Stable Diffusion session with simple yaml we provide. 🚀",
+    'One major hurdle in machine learning projects is establishing an environment. <a href="https://vessl.ai/floyd">VESSL AI</a> provides a solution to this bottleneck through YAML configuration. Using YAML configuration for a machine learning can offer a number of benefits:',
+    '♻️ <strong>Reproducibility</strong>: Clearly define and save configurations as a file ensures that your experiments can be reproduced exactly.',
+    '😉 <strong>Ease of Use</strong>: YAML files use a straightforward text format. This makes it easy for you to understand and modify the configurations as needed',
+    '🚀 <strong>Scalability</strong>: A consistent method of using YAML files can be easily version-controlled, shared, and reused, which simplifies scaling.',
+    'Try your Stable Diffusion session using the simple yaml we offer.',
 ]
 
-st.title("VESSL AI: Manage your own Stable Diffusion session!")
+st.title("Manage your own Stable Diffusion session!")
 for e in intro:
-    _e = f'<p style="font-family:Courier; color:Black; font-size: 20px;">{e}</p>'
+    _e = f'<p style="font-family:system-ui; color:Black; font-size: 20px;">{e}</p>'
     st.markdown(_e, unsafe_allow_html=True)
 
 
@@ -31,7 +31,7 @@ pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
 with st.form("prompt", clear_on_submit=False):
-    prompt = st.text_area("Write down your prompt here!: ", value="")
+    prompt = st.text_area("Write down your prompt here: ", value="")
     submit_button = st.form_submit_button(label="Enter")
     if submit_button:
         image = pipe(prompt).images[0]
@@ -46,7 +46,7 @@ col4, col5 = st.columns(2)
 with col4:
     yaml = """name : stable-diffusion
 resources:
-  cluster: aws-apne2-prod1
+  cluster: aws-uw2-prod1
   accelerators: V100:1
 image: quay.io/vessl-ai/ngc-pytorch-kernel:22.12-py3-202301160809
 run:
@@ -61,18 +61,18 @@ interactive:
     - 8501
     """
     st.markdown(
-        f'<p style="font-family:Courier; color:Black; font-size: 20px;">Here, we provide the yaml we used for setting up this streamlit session.</p>',
+        f'<p style="font-family:system-ui; color:Black; font-size: 20px;">Here is the YAML we used for setting up this streamlit session.</p>',
         unsafe_allow_html=True,
     )
     st.code(yaml, language="yaml", line_numbers=False)
 with col5:
     st.markdown(
-        f'<p style="font-family:Courier; color:Black; font-size: 20px;">You can save the yaml and run it by userself! Try:</p>',
+        f'<p style="font-family:system-ui; color:Black; font-size: 20px;">You can save the YAML into a file and run it by yourself! Try:</p>',
         unsafe_allow_html=True,
     )
-    st.code("pip install vessl\nvessl run -f youryaml", language="python", line_numbers=False)
+    st.code("pip install vessl\nvessl run -f stable-diffusion.yaml", language="bash", line_numbers=False)
 
 st.markdown(
-    f'<p style="font-family:Courier; color:Black; font-size: 20px;">For further details, visit <a href="https://vessl.ai/">link!</a>',
+    f'<p style="font-family:system-ui; color:Black; font-size: 20px;">For further details, visit <a href="https://vesslai.mintlify.app/docs/reference/yaml">VESSL Run Docs</a>.',
     unsafe_allow_html=True,
 )
