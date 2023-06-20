db6948e142647f35ac1a1e7d6ea314b464721102
Floyd Ryoo <floyd@vessl.ai>
2023-06-19 19:39:51 -0700

    Update question_answering text

--- langchain/question_answering/demo.py
+++ langchain/question_answering/demo.py
@@ -28,16 +28,16 @@ def MakingQA(repo_url=None, branch="main", files=None, chunk_size=500, chunk_ove
 def streamlit_display_title():
     st.image(VESSL_LOGO_URL, width=400)
     intro = [
-        "VESSL AI: Manage your own Stable Diffusion session!",
-        "Setting environment is one of the biggest bottleneck for machine learning projects. 😥",
-        "VESSL AI lets you overcome the bottleneck with the use of yaml. 📋",
-        "By providing yaml, you can declaratively run your machine learning projects in reliable manner!",
-        "Try your own Stable Diffusion session with simple yaml we provide. 🚀",
+        'One major hurdle in machine learning projects is establishing an environment. <a href="https://vessl.ai/floyd">VESSL AI</a> provides a solution to this bottleneck through YAML configuration. Using YAML configuration for a machine learning can offer a number of benefits:',
+        '♻️ <strong>Reproducibility</strong>: Clearly define and save configurations as a file ensures that your experiments can be reproduced exactly.',
+        '😉 <strong>Ease of Use</strong>: YAML files use a straightforward text format. This makes it easy for you to understand and modify the configurations as needed',
+        '🚀 <strong>Scalability</strong>: A consistent method of using YAML files can be easily version-controlled, shared, and reused, which simplifies scaling.',
+        "Try your own LangChain session with simple yaml we provide.",
     ]
 
-    st.title("VESSL AI: Manage your own Langchain session!")
+    st.title("Manage your own Langchain session!")
     for e in intro:
-        _e = f'<p style="font-family:Courier; color:Black; font-size: 20px;">{e}</p>'
+        _e = f'<p style="font-family:system-ui; color:Black; font-size: 20px;">{e}</p>'
         st.markdown(_e, unsafe_allow_html=True)
     return
 
@@ -73,7 +73,7 @@ def main():
         )
         chunk_size = st.number_input("Enter chunk size [How large to split long texts]", value=500)
         chunk_overlap = st.number_input(
-            "Enter chunk overlap [How much to overlap splitted texts]", value=100
+            "Enter chunk overlap [How much to overlap split texts]", value=100
         )
         submit_button = st.button("Submit", key="submit_button")
         if submit_button and (uploaded_files or repo_url) and chunk_size and chunk_overlap:
@@ -127,7 +127,7 @@ def main():
             msg("Processing your question...", is_user=False)
             result = qa({"question": user_question, "chat_history": st.session_state["history"]})
         except:
-            msg("Error occured while processing, please retry with fresh question", is_user=False)
+            msg("Error occurred while processing, please retry with fresh question", is_user=False)
             return
         st.session_state["history"].append((user_question, result["answer"]))
         # display new answer
@@ -137,7 +137,7 @@ def main():
     with col4:
         yaml = """name : langchain
 resources:
-  cluster: aws-apne2-prod1
+  cluster: aws-uw2-prod1
   cpu: 1
   memory: 16Gi
   disk: 10Gi
@@ -154,19 +154,19 @@ interactive:
     - 8501
         """
         st.markdown(
-            f'<p style="font-family:Courier; color:Black; font-size: 20px;">Here, we provide the yaml we used for setting up this streamlit session.</p>',
+            f'<p style="font-family:system-ui; color:Black; font-size: 20px;">Here is the YAML we used for setting up this streamlit session.</p>',
             unsafe_allow_html=True,
         )
         st.code(yaml, language="yaml", line_numbers=False)
     with col5:
         st.markdown(
-            f'<p style="font-family:Courier; color:Black; font-size: 20px;">You can save the yaml and run it by userself! Try:</p>',
+            f'<p style="font-family:system-ui; color:Black; font-size: 20px;">You can save the YAML into a file and run it by yourself! Try:</p>',
             unsafe_allow_html=True,
         )
-        st.code("pip install vessl\nvessl run -f youryaml", language="python", line_numbers=False)
+        st.code("pip install vessl\nvessl run -f langchain.yaml", language="python", line_numbers=False)
 
     st.markdown(
-        f'<p style="font-family:Courier; color:Black; font-size: 20px;">For further details, visit <a href="https://vessl.ai/">this link!</a>',
+        f'<p style="font-family:system-ui; color:Black; font-size: 20px;">For further details, visit <a href="https://vesslai.mintlify.app/docs/reference/yaml">VESSL Run Docs</a>',
         unsafe_allow_html=True,
     )
 
