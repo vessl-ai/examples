f489bbe123da382d5df61b48d6314f0c5cebd158
yongheel <97027715+yongheel@users.noreply.github.com>
2023-07-17 17:17:19 +0900

    Update main.py

--- stable-diffusion/main.py
+++ stable-diffusion/main.py
@@ -4,6 +4,7 @@ from diffusers import DPMSolverMultistepScheduler, StableDiffusionPipeline
 
 # Load model from Hugging Face Diffusers
 model_id = "stabilityai/stable-diffusion-2-1"
+
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
