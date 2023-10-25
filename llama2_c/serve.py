eaf3477d316819b8305d60373f1467127cade4a3
Floyd Ryoo <floyd@vessl.ai>
2023-10-24 23:35:09 -0500

    Take BaseModel instead of raw string

--- llama2_c/serve.py
+++ llama2_c/serve.py
@@ -1,11 +1,13 @@
 import uvicorn
 import subprocess
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-model_path = "/root/examples/llama2_c/stories42M.bin"
+model_path = "/Users/floydryoo/Documents/github/vessl-ai/examples/llama2_c/stories42M.bin"
 model = None
 
 
@@ -13,9 +15,13 @@ def prepare():
     subprocess.run(f"cd ./llama2_c && gcc -O3 -o run run.c -lm && chmod u+x {model_path}", shell=True)
 
 
+class PromptModel(BaseModel):
+    prompts: str
+
+
 @app.post("/generate/")
-async def generate(prompts: str):
-    command = f"cd ./llama2_c && ./run {model_path} -t 0.9 -n 256 -i \"{prompts}\""
+async def generate(p_model: PromptModel = Body(...)):
+    command = f"cd ./llama2_c && ./run {model_path} -t 0.9 -n 256 -i \"{p_model.prompts}\""
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
 
