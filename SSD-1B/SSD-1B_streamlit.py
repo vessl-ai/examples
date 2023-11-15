4e379f70a1036247abe84bb9b275ad088ac5b21f
mosuhy <32814164+mosuhy@users.noreply.github.com>
2023-11-15 16:26:39 +0900

    Add files via upload

---
+++ SSD-1B/SSD-1B_streamlit.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import os, sys
+import subprocess
+from PIL import Image
+from diffusers import StableDiffusionXLPipeline
+import torch
+pipe = StableDiffusionXLPipeline.from_pretrained("/data/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
+
+st.set_page_config(layout="wide")
+
+image_path = "./An astronaut riding a green horse.jpg"
+
+col1, col2 = st.columns(2)
+
+with col1:
+    input_img = Image.open(image_path)
+    st.image(input_img)
+with col2:
+    prompt = st.text_input('Input Query', 'Tom see flowers')
+    bind_socket()
+        
+    prompt = "An astronaut riding a green horse" # Your prompt here
+    neg_prompt = "ugly, blurry, poor quality, scary" # Negative prompt here
+    image = pipe(prompt=prompt, negative_prompt=neg_prompt).images[0]
+    image.save(prompt + '.jpg')
+    
+    print(output_sentences)
+    st.write(output_sentences)
