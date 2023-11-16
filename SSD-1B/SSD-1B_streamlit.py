9e0f55ab26c9a90a3489b47d5a3dfe01a7f7be38
mosuhy <32814164+mosuhy@users.noreply.github.com>
2023-11-16 13:22:07 +0900

    Update SSD-1B_streamlit.py

--- SSD-1B/SSD-1B_streamlit.py
+++ SSD-1B/SSD-1B_streamlit.py
@@ -5,28 +5,22 @@ from PIL import Image
 from diffusers import StableDiffusionXLPipeline
 import torch
 
-pipe = StableDiffusionXLPipeline.from_pretrained("/data/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
-pipe.to("cuda")
-
 # To run streamlit, setup streamlit port as 80 and run 'streamlit run SSD-1B_streamlit.py --server.port=80'
 
 st.set_page_config(layout="wide")
 
-image_path = "./An astronaut riding a green horse.jpg"
+pipe = StableDiffusionXLPipeline.from_pretrained("/data/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
 
-col1, col2 = st.columns(2)
+Input_query = "An astronaut riding a green horse" # Your prompt here
+prompt = st.text_input('Input Query', Input_query)
+neg_prompt = "ugly, blurry, poor quality, scary" # Negative prompt here
+image = pipe(prompt=prompt, negative_prompt=neg_prompt).images[0]
 
-with col1:
-    input_img = Image.open(image_path)
-    st.image(input_img)
-with col2:
-    prompt = st.text_input('Input Query', 'Tom see flowers')
-    bind_socket()
-        
-    prompt = "An astronaut riding a green horse" # Your prompt here
-    neg_prompt = "ugly, blurry, poor quality, scary" # Negative prompt here
-    image = pipe(prompt=prompt, negative_prompt=neg_prompt).images[0]
-    image.save(prompt + '.jpg')
-    
-    print(output_sentences)
-    st.write(output_sentences)
+process = f"python SSD-1B_inference.py --prompt '{prompt}'"
+
+generated_image = prompt + '.jpg'
+image.save(generated_image)
+image_path = "./" + generated_image
+generated_img = Image.open(image_path)
+st.image(generated_img)
