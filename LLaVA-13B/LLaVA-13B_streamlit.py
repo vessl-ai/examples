dac0ab5f8768363347793f0aba02e364fad3b4c6
mosuhy <32814164+mosuhy@users.noreply.github.com>
2023-11-22 15:23:49 +0900

    Add files via upload

---
+++ LLaVA-13B/LLaVA-13B_streamlit.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import os, sys
+import subprocess
+from PIL import Image
+import torch
+
+
+from transformers import AutoTokenizer, BitsAndBytesConfig
+from llava.model import LlavaLlamaForCausalLM
+from llava.mm_utils import (
+    KeywordsStoppingCriteria,
+    get_model_name_from_path,
+    process_images,
+    tokenizer_image_token,
+)
+import torch
+import textwrap
+
+
+# To run streamlit, setup streamlit port as 80 and run 'streamlit run LLaVA-13B_streamlit.py --server.port=80'
+st.set_page_config(layout="wide")
+
+model_path = "4bit/llava-v1.5-13b-3GB"
+kwargs = {"device_map": "auto"}
+kwargs['load_in_4bit'] = True
+kwargs['quantization_config'] = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type='nf4'
+)
+model = LlavaLlamaForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
+
+vision_tower = model.get_vision_tower()
+if not vision_tower.is_loaded:
+    vision_tower.load_model()
+vision_tower.to(device='cuda')
+image_processor = vision_tower.image_processor
+
+
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.utils import disable_torch_init
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
+from transformers import TextStreamer
+
+
+def caption_image_from_internet(image_file, prompt):
+    if image_file.startswith('http') or image_file.startswith('https'):
+        response = requests.get(image_file)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(image_file).convert('RGB')
+    disable_torch_init()
+    conv_mode = "llava_v0"
+    conv = conv_templates[conv_mode].copy()
+    roles = conv.roles
+    image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().cuda()
+    inp = f"{roles[0]}: {prompt}"
+    inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+    conv.append_message(conv.roles[0], inp)
+    conv.append_message(conv.roles[1], None)
+    raw_prompt = conv.get_prompt()
+    input_ids = tokenizer_image_token(raw_prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+    keywords = [stop_str]
+    stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+    with torch.inference_mode():
+      output_ids = model.generate(input_ids, images=image_tensor, do_sample=True, temperature=0.2,
+                                  max_new_tokens=1024, use_cache=True, stopping_criteria=[stopping_criteria])
+    outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+    conv.messages[-1][-1] = outputs
+    output = outputs.rsplit('</s>', 1)[0]
+    return image, output
+
+def caption_image_from_upload(image_file, prompt):
+    image = Image.open(image_file).convert('RGB')
+    disable_torch_init()
+    conv_mode = "llava_v0"
+    conv = conv_templates[conv_mode].copy()
+    roles = conv.roles
+    image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'].half().cuda()
+    inp = f"{roles[0]}: {prompt}"
+    inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+    conv.append_message(conv.roles[0], inp)
+    conv.append_message(conv.roles[1], None)
+    raw_prompt = conv.get_prompt()
+    input_ids = tokenizer_image_token(raw_prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+    keywords = [stop_str]
+    stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+    with torch.inference_mode():
+      output_ids = model.generate(input_ids, images=image_tensor, do_sample=True, temperature=0.2,
+                                  max_new_tokens=1024, use_cache=True, stopping_criteria=[stopping_criteria])
+    outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+    conv.messages[-1][-1] = outputs
+    output = outputs.rsplit('</s>', 1)[0]
+    return image, output
+
+
+Input_query = 'Describe the image and color details.'
+prompt = st.text_input('Input Query', Input_query)
+
+image_file = st.file_uploader('이미지를 업로드 하세요.', type=['png', 'jpg', 'jpeg'])
+if image_file is not None:
+    image, output = caption_image_from_upload(image_file, prompt)
+else:
+    image, output = caption_image_from_internet(f'https://llava-vl.github.io/static/images/view.jpg', prompt)
+
+
+st.image(image)
+st.write('Result: ', output)
