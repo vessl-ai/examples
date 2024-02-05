f8892cc9f29232a0a1c53f6a9764ee55da0230f7
Floyd Ryoo <floyd@vessl.ai>
2024-02-05 10:23:19 +0900

    Add tracking example

---
+++ tracking/main.py
@@ -0,0 +1,13 @@
+import os
+import random 
+import vessl
+
+epochs = os.environ['epochs']
+lr = os.environ['lr']
+offset = random.random() / 5
+
+for epoch in range(2, epochs):
+    acc = 1 - 2**-epoch - random.random() / epoch - offset
+    loss = 2**-epoch + random.random() / epoch + offset
+    print(f"epoch={epoch}, accuracy={acc}, loss={loss}")
+    vessl.log({"accuracy": acc, "loss": loss})
