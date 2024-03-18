fc454d582824c586e2718df252da81e8cf04efa9
Floyd Ryoo <floyd@vessl.ai>
2024-03-18 10:39:03 +0900

    Add tqdm

--- rag-chatbot/app.py
+++ rag-chatbot/app.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import shutil
+import tqdm
 from time import sleep
 from typing import Dict, Any, List, Optional
 
@@ -42,6 +43,7 @@ CHAT_TEMPLATE = """
 {%- endif -%}
 """
 
+
 def generate_vector_store_nodes(pdf_doc_path: str, embed_model: HuggingFaceEmbedding):
     loader = PyMuPDFReader()
     documents = loader.load(file_path=pdf_doc_path)
@@ -69,26 +71,28 @@ def generate_vector_store_nodes(pdf_doc_path: str, embed_model: HuggingFaceEmbed
     print(f"Embedded {len(nodes)} nodes from {pdf_doc_path}")
     return nodes
 
+
 class RAGInterface:
     def __init__(
-        self,
-        embedding_model_name: str,
-        docs_folder: str = "./docs",
-        stream: bool = False,
-        use_flash_attention: bool = False,
-        use_vllm: bool = True,
-        vllm_kwargs: Optional[Dict[str, Any]] = {},
+            self,
+            embedding_model_name: str,
+            docs_folder: str = "./docs",
+            stream: bool = False,
+            use_flash_attention: bool = False,
+            use_vllm: bool = True,
+            vllm_kwargs: Optional[Dict[str, Any]] = {},
     ):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.embed_model = HuggingFaceEmbedding(model_name=embedding_model_name, device=self.device)
 
         # FAISS vector store
-        self.faiss_index = faiss.IndexFlatL2(1024) # 1024 is dimension of the embeddings
+        self.faiss_index = faiss.IndexFlatL2(1024)  # 1024 is dimension of the embeddings
         self.vector_store = FaissVectorStore(faiss_index=self.faiss_index)
 
         # LlamaIndex Storage context to store nodes mapped to vector store
         self.storage_context = StorageContext.from_defaults(vector_store=self.vector_store)
-        self.vector_store_index = VectorStoreIndex(nodes=[], embed_model=self.embed_model, storage_context=self.storage_context)
+        self.vector_store_index = VectorStoreIndex(
+            nodes=[], embed_model=self.embed_model, storage_context=self.storage_context)
 
         self.docs_folder = docs_folder
         self.stream = stream
@@ -103,9 +107,10 @@ class RAGInterface:
 
         Parameters
         ----------
-        docs_folder : str
+        initial_docs: str
             Path to the folder containing the PDF documents.
-
+        model_name: str
+            Model name from huggingface.co/models in default or vllm if use_vllm is set to True
         """
 
         print(f"Initializing vector database from {len(initial_docs)} Documents...")
@@ -118,7 +123,7 @@ class RAGInterface:
             # Note: vLLM does not support streaming interface yet
             # ref: https://github.com/run-llama/llama_index/issues/9477
             print(f"Loading LLM from {model_name} using vLLM...")
-            from llama_index.llms.vllm import Vllm # lazy loading
+            from llama_index.llms.vllm import Vllm  # lazy loading
 
             llm = Vllm(
                 model=model_name,
@@ -131,7 +136,7 @@ class RAGInterface:
             )
         else:
             print(f"Loading LLM from {model_name} using transformers.AutoModelForCausalLM...")
-            from llama_index.llms.huggingface import HuggingFaceLLM # lazy loading
+            from llama_index.llms.huggingface import HuggingFaceLLM  # lazy loading
 
             model_kwargs = {"temperature": 0.8, "do_sample": True, "top_k": 10, "top_p": 0.95}
             if self.use_flash_attention:
@@ -157,16 +162,16 @@ class RAGInterface:
             return gr.update(value="Upload PDF documents", interactive=True)
 
         gr.Info(f"Adding {len(list_file_obj)} documents to vector database...")
-        progress(0, desc="Copying documents to ")
+        # progress(0, desc="Copying documents to ")
         pdf_docs = [x.name for x in list_file_obj if x is not None]
         for pdf in pdf_docs:
             shutil.copy(pdf, self.docs_folder)
 
         gr.Info("Adding documents into vector database...")
-        for pdf_file_path in pdf_docs:
+        for pdf_file_path in progress.tqdm(pdf_docs, desc="Copying documents to "):
             nodes = generate_vector_store_nodes(pdf_file_path, self.embed_model)
             self.vector_store_index.insert_nodes(nodes)
-            progress(1, desc=f"Adding {pdf_file_path} to vector database")
+            # progress(1, desc=f"Adding {pdf_file_path} to vector database")
 
         gr.Info("Upload Completed!")
         return gr.update(value="Upload PDF documents", interactive=True)
@@ -201,7 +206,7 @@ def main(args: argparse.Namespace):
     for root, _, files in os.walk(args.docs_folder):
         for file in files:
             if file.endswith(".pdf"):
-                initial_docs.append(os.path.join(root, file))
+                initial_docs.append(str(os.path.join(root, file)))
 
     ragger = RAGInterface(
         embedding_model_name=args.embedding_model_name,
@@ -219,11 +224,13 @@ def main(args: argparse.Namespace):
     with gr.Blocks(css=css, title="RAG Chatbot with LlamaIndex🦙 and Open-source LLMs") as demo:
         with gr.Row():
             gr.Markdown(
-            f"""<h2>RAG Chatbot with LlamaIndex🦙 and {args.model_name}</h2>
-            <h3>Ask any questions about your PDF documents, along with follow-ups</h3>
-            <b>Note:</b> This AI assistant performs retrieval-augmented generation from your PDF documents.<br>
-            Initial documents are loaded from the `{args.docs_folder}` folder. You can add more documents by clicking the button below.<br>
-            """)
+                f"""<h2>RAG Chatbot with LlamaIndex🦙</h2>
+                    <h2>Model name: {args.model_name}</h2>
+                    <h3>Ask any questions about your PDF documents, along with follow-ups</h3>
+                    <b>Note:</b> This AI assistant performs retrieval-augmented generation from your PDF documents.<br>
+                    Initial documents are loaded from the `{args.docs_folder}` folder. You can add more documents by 
+                    clicking the button below.<br>
+                """)
         with gr.Row():
             with gr.Column(scale=1):
                 with gr.Row():
@@ -233,13 +240,20 @@ def main(args: argparse.Namespace):
                         file_count="multiple",
                         file_types=["pdf"],
                         interactive=True,
-                        label="Add your PDF documents (single or multiple)")
+                        label="Add your PDF documents (single or multiple)"
+                    )
                 with gr.Row():
                     generate_vectordb_btn = gr.Button("Upload PDF documents")
                     generate_vectordb_btn.click(
-                        fn=lambda: gr.update(value="Uploading...", interactive=False), outputs=[generate_vectordb_btn]
+                        fn=lambda: gr.update(
+                            value="Uploading...",
+                            interactive=False
+                        ), outputs=[generate_vectordb_btn]
                     ).then(
-                        fn=ragger.add_document, inputs=[document], outputs=[generate_vectordb_btn])
+                        fn=ragger.add_document,
+                        inputs=[document],
+                        outputs=[generate_vectordb_btn]
+                    )
             with gr.Column(scale=2):
                 gr.ChatInterface(ragger.handle_chat)
         with gr.Row():
@@ -248,28 +262,34 @@ def main(args: argparse.Namespace):
 
     demo.queue().launch(server_name="0.0.0.0")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="RAG Chatbot",
         description="Question Answering with Retrieval QA and LangChain Language Models featuring FAISS vector stores.")
 
     parser.add_argument("--docs-folder", default="./docs", help="Path to the folder containing the PDF documents.")
-    parser.add_argument("--embedding-model-name", default="BAAI/bge-m3", help="HuggingFace model name for text embeddings.")
-    parser.add_argument("--model-name", default="TheBloke/Mistral-7B-Instruct-v0.2-AWQ", help="HuggingFace model name for LLM.")
+    parser.add_argument("--embedding-model-name", default="BAAI/bge-m3",
+                        help="HuggingFace model name for text embeddings.")
+    parser.add_argument("--model-name", default="TheBloke/Mistral-7B-Instruct-v0.2-AWQ",
+                        help="HuggingFace model name for LLM.")
     parser.add_argument("--use-vllm", action="store_true", help="Use vLLM for generation.")
     parser.add_argument("--stream", action="store_true", help="Use streaming interface for vLLM generation.")
     parser.add_argument("--use-flash-attention", action="store_true", help="Use Flash Attention for vLLM generation.")
     parser.add_argument("--vllm-max-model-len", default=4096, help="Max model length - Only active if using vLLM")
-    parser.add_argument("--vllm-enforce-eager", action="store_true", help="Enforce eager mode - Only active if using vLLM")
+    parser.add_argument("--vllm-enforce-eager", action="store_true",
+                        help="Enforce eager mode - Only active if using vLLM")
 
     args = parser.parse_args()
 
     # Validate arguments
     if args.use_vllm:
-        print("[WARN] Using vLLM for text generation. You might want to install vLLM with `pip install -U vllm llama-index-llms-vllm` before running the app.")
+        print(
+            "[WARN] Using vLLM for text generation. You might want to install vLLM with `pip install -U vllm llama-index-llms-vllm` before running the app.")
         if args.stream:
             raise ValueError("vLLM on LlamaIndex does not streaming interface yet. Please remove --stream flag.")
     if args.use_flash_attention:
-        print("[WARN] Using Flash Attention for vLLM. You might want to install Flash Attention with `pip install -U flash-attn` before running the app.")
+        print(
+            "[WARN] Using Flash Attention for vLLM. You might want to install Flash Attention with `pip install -U flash-attn` before running the app.")
 
     main(args)
