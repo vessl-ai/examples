7350474ff9d95dda2e6cbdcd487dbf8722d5375c
Ian Lee <ian@vessl.ai>
2024-03-29 16:54:10 +0900

    add version info in ssd-1b, stable-diffusion

--- stable-diffusion/setup.sh
+++ stable-diffusion/setup.sh
@@ -1,5 +1,5 @@
-pip install vessl
-pip install streamlit
-pip install diffusers
-pip install accelerate
-pip install transformers
+pip install vessl==0.1.142
+pip install streamlit==1.32.2
+pip install diffusers==0.27.2
+pip install accelerate==0.28.0
+pip install transformers==4.39.1
