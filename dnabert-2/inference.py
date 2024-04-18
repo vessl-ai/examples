b75a12265396c16046c25d3cb5bd7ea507117a3c
Ian Lee <ian@vessl.ai>
2024-04-18 14:20:34 +0900

    add gradio web app script

---
+++ dnabert-2/inference.py
@@ -0,0 +1,20 @@
+import os
+
+import gradio as gr
+import torch
+import transformers
+
+model_path = os.get("MODEL_PATH", "/model")
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path)
+
+def infer(dna: str) -> int:
+    inputs = tokenizer(dna, return_tensors="pt")["input_ids"]
+    with torch.no_grad():
+        outputs = model(inputs).logits
+    predicted_class = torch.argmax(outputs[0]).item()
+    return predicted_class
+
+iface = gr.Interface(fn=infer, inputs='text', outputs='text')
+iface.launch(server_name="0.0.0.0")
