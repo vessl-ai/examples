f79c5630e5ab7c24118e4d32361190de6fbbee24
Sanghyuk Lee <sanghyuk@vessl.ai>
2024-05-02 23:13:18 +0900

    Update face-to-sticker model

--- deprecated/face-to-sticker/app.py
+++ deprecated/face-to-sticker/app.py
@@ -5,9 +5,8 @@ from io import BytesIO
 import boto3
 import streamlit as st
 from botocore.exceptions import ClientError
-from PIL import Image
-
 from model import get_antelope_app, load_model, run_pipe
+from PIL import Image
 
 
 def send_email(html):
@@ -79,22 +78,32 @@ with st.sidebar:
         "negative prompt",
         value="photo, photography, soft, nsfw, nude, ugly, broken, watermark, oversaturated",
     )
-    # strength = st.slider(
-    #     "denoising strength", min_value=0.0, max_value=1.0, step=0.01, value=0.4
-    # )
     guidance = st.slider("cfg", min_value=1.0, max_value=20.0, step=0.1, value=7.5)
-    steps = st.number_input("inference steps", 10, 200, step=1, value=20)
+    steps = st.number_input("inference steps", 10, 200, step=1, value=10)
     image_size = st.number_input(
         "image size", min_value=512, max_value=768, step=64, value=768
     )
-    num_photos = st.number_input("number of photos", 1, 4, step=1, value=4)
+    num_photos = st.number_input("number of photos", 1, 3, step=1, value=3)
+
+
+if "stage" not in st.session_state:
+    st.session_state.stage = 0
+
+
+if "images" not in st.session_state:
+    st.session_state.images = None
+
+
+def image_on_click():
+    st.session_state.stage = 1
+    st.session_state.images = None
 
 
 st.header("Upload image or take your photo!")
 with st.form("image", clear_on_submit=False):
     image_path = st.file_uploader("Upload your image", type=["png", "jpg", "jpeg"])
     img_file_buffer = st.camera_input("Take your photo!")
-    submit_button = st.form_submit_button(label="Generate")
+    submit_button = st.form_submit_button(label="Generate", on_click=image_on_click)
 
     os.makedirs("./temp", exist_ok=True)
 
@@ -105,7 +114,7 @@ with st.form("image", clear_on_submit=False):
         img = Image.open(image_path)
         img.save(TEMP_PATH, "PNG")
 
-if submit_button:
+if st.session_state.stage >= 1:
     st.header("Result Image")
 
     input_image = Image.open(TEMP_PATH)
@@ -114,32 +123,49 @@ if submit_button:
 
     input_image = crop_and_resize(input_image, image_size)
 
-    with st.spinner("generating images..."):
-        images = run_pipe(
-            pipe,
-            app,
-            input_image,
-            prompt,
-            negative_prompt,
-            num_inference_steps=steps,
-            guidance_scale=guidance,
-            num_images_per_prompt=num_photos,
+    if st.session_state.images is None:
+        with st.spinner("generating images..."):
+            images = run_pipe(
+                pipe,
+                app,
+                input_image,
+                prompt,
+                negative_prompt,
+                num_inference_steps=steps,
+                guidance_scale=guidance,
+                num_images_per_prompt=num_photos,
+            )
+            st.session_state.images = images
+    else:
+        images = st.session_state.images
+
+    columns = st.columns(num_photos)
+    for i in range(num_photos):
+        with columns[i]:
+            st.image(images[i])
+
+    with st.form("radio-button"):
+        option = st.radio(
+            "select an image", [i + 1 for i in range(num_photos)], horizontal=True
         )
+        option_submit = st.form_submit_button(label="Select")
 
-    stream = BytesIO()
-    email_image = images[0].resize((512, 512), Image.Resampling.LANCZOS)
-    email_image.save(stream, format="png")
-    stream.seek(0)
-    imgObj = stream.read()
-    b64img = base64.b64encode(imgObj)
-    mail = f"""<html>
-<body>
-<h1>Here is your sticker!</h1>
-<img src="data:image/png;base64,{b64img.decode()}" alt="sticker">
-</body>
-</html>"""
-    send_email(mail)
+    if option_submit:
+        stream = BytesIO()
+        idx = int(option) - 1
+        email_image = images[idx].resize((512, 512), Image.Resampling.LANCZOS)
+        email_image.save(stream, format="png")
+        stream.seek(0)
+        imgObj = stream.read()
+        b64img = base64.b64encode(imgObj)
+        mail = f"""<html>
+    <body>
+    <h1>Here is your sticker!</h1>
+    <img src="data:image/png;base64,{b64img.decode()}" alt="sticker">
+    </body>
+    </html>"""
+        send_email(mail)
 
-    st.image(images)
-
-    # send_email_button = st.button("Send email")
+        st.toast("Email sent!")
+        st.session_state.stage = 0
+        st.session_state.images = None
