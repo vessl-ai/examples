6cf6d8fb74c8311750bb5a12cd4360658e955573
Sanghyuk Lee <sanghyuk@vessl.ai>
2024-11-25 21:49:07 +0900

    Update output directory for DPO merge script

--- pipelines/alignment/2_dpo/merge.sh
+++ pipelines/alignment/2_dpo/merge.sh
@@ -2,5 +2,5 @@ peft_type=${FINETUNING_METHOD,,}
 
 python merge.py --model-name-or-path /root/model \
     --adapter-name-or-path /root/training_results \
-    --output-dir /root/results \
+    --output-dir /root/results/dpo-volume \
     --peft $peft_type
