c1ed8666799bc497a53f1e5df0fc3cb6ff753cd2
Sanghyuk Lee <sanghyuk@vessl.ai>
2024-11-27 12:01:56 +0900

    Fix volume upload script

--- pipelines/alignment/5_upload/upload.py
+++ pipelines/alignment/5_upload/upload.py
@@ -2,33 +2,25 @@ import argparse
 from pathlib import Path
 
 import vessl
-from vessl.util.exception import VesslApiException
+import vessl.volume
 
 
-def main(path: Path, model_repo: str):
-    try:
-        _ = vessl.read_model_repository(model_repo)
-    except VesslApiException:
-        vessl.create_model_repository(model_repo)
-
-    model_info = vessl.create_model(model_repo)
-    model_number = model_info.number
-
+def main(path: Path, volume: str):
     for file_path in path.glob("*"):
-        relative_path = file_path.relative_to(path)
+        relative_path = str(file_path.relative_to(path))
 
-        vessl.upload_model_volume_file(
-            model_repo,
-            model_number,
-            str(file_path),
-            str("/" / relative_path),
+        source_ref = vessl.volume.parse_volume_url(
+            str(file_path), raise_if_not_exists=True
         )
+        dest_ref = vessl.volume.parse_volume_url(volume + f"/{relative_path}")
+
+        vessl.volume.copy_volume_file_v2(source_ref, dest_ref)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type=Path, required=True)
-    parser.add_argument("--model-repo", type=str, required=True)
+    parser.add_argument("--volume", type=str, required=True)
     args = parser.parse_args()
 
     main(args.path, args.model_repo)
