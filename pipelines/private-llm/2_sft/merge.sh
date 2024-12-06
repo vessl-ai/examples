ceeab4565f9a67aa3b2036f1de85452bb1372213
Sanghyuk Lee <sanghyuk@vessl.ai>
2024-12-06 14:43:32 +0900

    Remove cleanup code

--- pipelines/private-llm/2_sft/merge.sh
+++ pipelines/private-llm/2_sft/merge.sh
@@ -4,6 +4,3 @@ python merge.py --model-name-or-path /root/model \
     --adapter-name-or-path /root/results/sft_intermediate \
     --output-dir /root/results/sft_model \
     --peft $peft_type
-
-# cleanup intermediate results
-rm -rf /root/results/sft_intermediate
