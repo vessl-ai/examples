5851f1e94e804ea739420552e1c3c3e4fbfb35fb
Ian Lee <ian@vessl.ai>
2025-01-14 18:59:37 +0900

    add stop button to yolo gradio

--- runs/YOLO11/predict/app.py
+++ runs/YOLO11/predict/app.py
@@ -1,5 +1,7 @@
 import argparse
+import os
 import PIL.Image as Image
+from time import sleep
 
 import gradio as gr
 from ultralytics import YOLO
@@ -23,18 +25,46 @@ def predict_image(img, conf_threshold, iou_threshold):
     return im
 
 
+def close_app():
+    gr.Info("Terminated the app!")
+    sleep(1)
+    os._exit(0)
+
+
 def main(port: int):
-    demo = gr.Interface(
-        fn=predict_image,
-        inputs=[
-            gr.Image(type="pil", label="Upload Image"),
-            gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
-            gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
-        ],
-        outputs=gr.Image(type="pil", label="Result"),
-        title="Ultralytics Gradio",
-        description="Upload images for inference.",
-    )
+    with gr.Blocks(title="Ultralytics YOLO Gradio") as demo:
+        with gr.Row():
+            gr.Markdown(
+                """<h2>Ultralytics YOLO</h2>
+                <h3>Upload an image for inference with Ultralytics YOLO.</h3>"""
+            )
+        with gr.Row():
+            with gr.Column(scale=1):
+                with gr.Row():
+                    input_image = gr.Image(type="pil", label="Upload Image")
+                with gr.Row():
+                    with gr.Accordion("Advanced options", open=True):
+                        confidence_slider = gr.Slider(
+                            minimum=0, maximum=1, value=0.25, label="Confidence threshold"
+                        )
+                        iou_slider = gr.Slider(
+                            minimum=0, maximum=1, value=0.45, label="IoU threshold"
+                        )
+                with gr.Row():
+                    submit_btn = gr.Button("Submit", variant="primary")
+            with gr.Column(scale=1):
+                with gr.Row():
+                    output_image = gr.Image(type="pil", label="result")
+        with gr.Row():
+            close_button = gr.Button("Close the app", variant="stop")
+        
+        submit_btn.click(
+            predict_image,
+            inputs=[input_image, confidence_slider, iou_slider],
+            outputs=[output_image],
+        )
+        close_button.click(fn=lambda: gr.update(interactive=False), outputs=[close_button]).then(fn=close_app)
+
     demo.launch(server_name="0.0.0.0", server_port=port)
 
 
