c50e621ed4aa60596471972f6b94dd65ecb17358
Ian Lee <ian@vessl.ai>
2025-02-04 16:45:25 +0900

    delete unused import and fuctions, change chatbot message type, disable markdown rendering

--- runs/hf-chatbot-vllm/app.py
+++ runs/hf-chatbot-vllm/app.py
@@ -1,11 +1,12 @@
 import argparse
-import os
-from time import sleep
-from typing import List, Optional
-from threading import Thread
+from typing import Optional
 
 import gradio as gr
 from transformers import AutoTokenizer
+from vllm import SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.utils import random_uuid
 
 
 class LLMChatHandler():
@@ -15,8 +16,6 @@ class LLMChatHandler():
             self.tokenizer.eos_token_id,
             self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
         ]
-        from vllm.engine.arg_utils import AsyncEngineArgs
-        from vllm.engine.async_llm_engine import AsyncLLMEngine
 
         def _guess_quantization(model_id) -> Optional[str]:
             if "awq" in model_id or "AWQ" in model_id:
@@ -44,28 +43,10 @@ class LLMChatHandler():
         )
         self.vllm_engine = AsyncLLMEngine.from_engine_args(engine_args)
 
-    def chat_history_to_prompt(self, message: str, history: List[List[str]]) -> str:
-        conversation = []
-        for h in history:
-            user_text, assistant_text = h
-            conversation += [
-                {"role": "user", "content": user_text},
-                {"role": "assistant", "content": assistant_text},
-            ]
-
-        conversation.append({"role": "user", "content": message})
-        prompt = self.tokenizer.apply_chat_template(conversation, tokenize=False, add_generation_prompt=True)
-        return prompt
 
     async def chat_function(self, message, history):
-        prompt = self.chat_history_to_prompt(message, history)
-        response_generator = self.chat_function_vllm(prompt)
-        async for text in response_generator:
-            yield text
-
-    async def chat_function_vllm(self, prompt):
-        from vllm import SamplingParams
-        from vllm.utils import random_uuid
+        history.append({"role": "user", "content": message})
+        prompt = self.tokenizer.apply_chat_template(history, tokenize=False, add_generation_prompt=True)
         sampling_params = SamplingParams(
             stop_token_ids=self.terminators,
             max_tokens=2048,
@@ -80,31 +61,6 @@ class LLMChatHandler():
                     response_txt += output.text
             yield response_txt
 
-    def chat_function_hf(self, prompt):
-        from transformers import pipeline, TextIteratorStreamer
-        streamer = TextIteratorStreamer(self.tokenizer)
-        pipe = pipeline(
-            "text-generation",
-            model=self.hf_model,
-            tokenizer=self.tokenizer,
-            eos_token_id=self.terminators,
-            max_length=2048,
-            temperature=0.6,
-            top_p=0.9,
-            repetition_penalty=1.2,
-            return_full_text=False,
-            streamer=streamer
-        )
-        t = Thread(target=pipe, args=(prompt,))
-        t.start()
-
-        generated_text = ""
-        for new_text in streamer:
-            generated_text += new_text
-            yield generated_text
-
-        t.join()
-
 
 def main(args):
     print(f"Loading the model {args.model_id}...")
@@ -116,7 +72,8 @@ def main(args):
                 f"<h2>Chatbot with 🤗 {args.model_id} 🤗</h2>"
                 "<h3>Interact with LLM using chat interface!<br></h3>"
                 f"<h3>Original model: <a href='https://huggingface.co/{args.model_id}' target='_blank'>{args.model_id}</a></h3>")
-        gr.ChatInterface(hdlr.chat_function)
+        cb = gr.Chatbot(type="messages", scale=20, render_markdown=False)
+        gr.ChatInterface(hdlr.chat_function, chatbot=cb)
 
     demo.queue().launch(server_name="0.0.0.0", server_port=args.port)
 
